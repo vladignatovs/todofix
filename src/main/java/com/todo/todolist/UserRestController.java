fe665168431e7986babb4faf778939fa01bffb3b
vladignatovs <131262207+vladignatovs@users.noreply.github.com>
2024-05-09 16:56:26 +0000

    resend_fix

--- src/main/java/com/todo/todolist/UserRestController.java
+++ src/main/java/com/todo/todolist/UserRestController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.todo.domain.Task;
 import com.todo.domain.Todolist;
 import com.todo.domain.User;
 import com.todo.repository.TaskRepository;
